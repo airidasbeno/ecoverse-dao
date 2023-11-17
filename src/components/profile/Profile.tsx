af28697324c8beb4218ddfb78dc790ef9e7a64f9
airidasbeno <airidas.beno@gmail.com>
2023-11-17 22:22:55 +0300

    Fixes

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Information, Status} from "./partials";
-import {useWeb3React} from "@web3-react/core";
 
 const Profile: React.FC = () => {
     return (
@@ -9,8 +7,6 @@ const Profile: React.FC = () => {
             <p>Welcome to our unique shopping platform. Experience a whole new world of online shopping where quality meets
                 convenience. Explore our vast range of products, savor exclusive discounts, and enjoy seamless browsing.
                 Your satisfaction is our promise!</p>
-            <Status isActivating={isActivating} isActive={isActive} />
-            <Information chainId={chainId} />
         </>
         
     );
