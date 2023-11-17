fa277ea48d6794347bb2b5c5323d52541788cc47
airidasbeno <airidas.beno@gmail.com>
2023-11-18 00:27:33 +0300

    Wallet connect fixes

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {useAccount} from "wagmi";
 
 const Profile: React.FC = () => {
+    const { address, isConnected } = useAccount();
+
     return (
         <>
             <h1>Profile</h1>
             <p>Welcome to our unique shopping platform. Experience a whole new world of online shopping where quality meets
                 convenience. Explore our vast range of products, savor exclusive discounts, and enjoy seamless browsing.
                 Your satisfaction is our promise!</p>
+            <p>Status: {isConnected ? 'Connected' : 'Not Connected'}</p>
+            <p>Address: {address}</p>
         </>
-        
     );
 };
+
 export default Profile;
