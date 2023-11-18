d38cabef4770785198b248afea02b729cb707080
airidasbeno <airidas.beno@gmail.com>
2023-11-18 13:59:31 +0300

    Campaigns styling

--- src/components/campaigns/Campaigns.tsx
+++ src/components/campaigns/Campaigns.tsx
@@ -5,13 +5,15 @@ import MarketplaceCard from "./partials/CampaignCard";
 const Campaigns: React.FC = () => {
     return (
         <>
-            <h1 style={{ textAlign: 'center' }}>Your Campaigns</h1>
+            <h1>My Campaigns</h1>
+            <p>Welcome to our unique shopping platform. Experience a whole new world of online shopping where quality
+                meets convenience. Explore our vast range of products, savor exclusive discounts, and enjoy seamless browsing. Your satisfaction is our promise!</p>
             <Row>
-                <Col span={4}>
-                    <MarketplaceCard />
+                <Col span={6}>
+                    <MarketplaceCard/>
                 </Col>
-                <Col span={4}>
-                    <MarketplaceCard />
+                <Col span={6}>
+                    <MarketplaceCard/>
                 </Col>
             </Row>
         </>
