e3e90fbad7b455fb3637d701923a22cab9b8b051
airidasbeno <airidas.beno@gmail.com>
2023-11-18 21:50:21 +0300

    Change forms styling

--- src/components/campaigns/partials/SubmitCampaign.tsx
+++ src/components/campaigns/partials/SubmitCampaign.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Col, Form, InputNumber, message, Row} from 'antd';
+import {Button, Card, Col, Form, InputNumber, message, Row} from 'antd';
 import ProgressSteps from "./ProgressSteps";
 
 const styles = {
@@ -40,34 +40,32 @@ const SubmitCampaign: React.FC = () => {
         <>
             {contextHolder}
             <ProgressSteps step={2}/>
-            <Row>
-                <Col span={12} offset={6}>
-                    <h1>Add to Marketplace</h1>
-                    <p>Welcome to our unique shopping platform. Experience a whole new world of online shopping where
-                        quality meets convenience. Explore our vast range of products
-                    </p>
-                    <Form
-                        name="basic"
-                        labelCol={{span: 6}}
-                        wrapperCol={{span: 20}}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item<FieldType>
-                            label="Project Budget"
-                            name="budget"
-                            rules={[{required: true, message: 'Input project budget'}]}
+            <Row style={{ marginTop: '30px' }}>
+                <Col span={18} offset={3}>
+                    <Card title="Add Campaign to Marketplace" bordered={true}>
+                        <Form
+                            name="basic"
+                            labelCol={{span: 4}}
+                            wrapperCol={{span: 18}}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
                         >
-                            <InputNumber prefix="$" style={{width: '100%'}}/>
-                        </Form.Item>
+                            <Form.Item<FieldType>
+                                label="Project Budget"
+                                name="budget"
+                                rules={[{required: true, message: 'Input project budget'}]}
+                            >
+                                <InputNumber prefix="$" style={{width: '100%'}}/>
+                            </Form.Item>
 
-                        <Form.Item wrapperCol={{offset: 12, span: 12}}>
-                            <Button type="primary" htmlType="submit" style={styles.button}>
-                                Submit Campaign
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item wrapperCol={{ offset: 10, span: 12 }} style={{ marginTop: '40px' }}>
+                                <Button type="primary" htmlType="submit" style={styles.button}>
+                                    Submit Campaign
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
                 </Col>
             </Row>
         </>
