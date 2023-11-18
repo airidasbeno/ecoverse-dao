8753951f45931af35520b73230425e535357355a
gudzineviciusv <vytautas+1@bitdegree.org>
2023-11-18 14:37:59 +0300

    Feature - added marketplace hook

--- src/hooks/useMint.ts
+++ src/hooks/useMint.ts
@@ -1,11 +1,8 @@
 import { ethers } from 'ethers';
 
-const contractABI: ethers.ContractInterface = [
-    // Add the ABI of your contract here
-    // Make sure to include the updated mintUniqueToken function's ABI
-];
+import contractAbi from '../assets/abis/mint.json';
 
-const contractAddreses: { [key: number]: string } = {
+export const contractAddreses: { [key: number]: string } = {
     5: "0xd9145CCE52D386f254917e481eB44e9943F39138",
     111: "",
 };
@@ -15,7 +12,7 @@ const provider = new ethers.providers.Web3Provider(window.ethereum as unknown as
 const network = await provider.getNetwork();
 const signer: ethers.Signer | ethers.providers.Provider | undefined = provider.getSigner();
 
-    const contract = new ethers.Contract(contractAddreses[network.chainId], contractABI, signer);
+    const contract = new ethers.Contract(contractAddreses[network.chainId], contractAbi, signer);
 
     const mintUniqueToken = async (to: string, amount: number, tokenURI: string, data?: string) => {
         try {
