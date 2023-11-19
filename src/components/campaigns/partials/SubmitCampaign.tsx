88298ad7060fd89d5753f7d8516487b4955f3a34
airidasbeno <airidas.beno@gmail.com>
2023-11-19 03:09:01 +0300

    Add connect to submit campaign page

--- src/components/campaigns/partials/SubmitCampaign.tsx
+++ src/components/campaigns/partials/SubmitCampaign.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Button, Card, Col, Form, InputNumber, message, Row} from 'antd';
+import { Button, Card, Col, Form, InputNumber, message, Row } from 'antd';
 import ProgressSteps from "./ProgressSteps";
-import {useAccount, useChainId} from "wagmi";
+import { useAccount, useChainId } from "wagmi";
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 
 const styles = {
     button: {
@@ -22,6 +23,8 @@ type FieldType = {
 
 const SubmitCampaign: React.FC = () => {
     const [messageApi, contextHolder] = message.useMessage();
+    const { isConnected } = useAccount();
+    const { open } = useWeb3Modal();
 
     const { connector } = useAccount();
     const connectedChainId = useChainId();
@@ -48,35 +51,53 @@ const SubmitCampaign: React.FC = () => {
     return (
         <>
             {contextHolder}
-            <ProgressSteps step={2}/>
-            <Row style={{ marginTop: '30px' }}>
-                <Col span={18} offset={3}>
-                    <Card title="Add Campaign to Marketplace" bordered={true}>
-                        <Form
-                            name="basic"
-                            labelCol={{span: 4}}
-                            wrapperCol={{span: 18}}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item<FieldType>
-                                label="Project Budget"
-                                name="budget"
-                                rules={[{required: true, message: 'Input project budget'}]}
-                            >
-                                <InputNumber prefix={connectedChain?.nativeCurrency.symbol} style={{width: '100%'}}/>
-                            </Form.Item>
+            <h1>Submit Campaign to Marketplace</h1>
+            <p>
+                Welcome to our unique shopping platform. Experience a whole new world of online shopping where
+                quality
+                meets convenience. Explore our vast range of products, savor exclusive discounts, and enjoy
+                seamless
+                browsing. Your satisfaction is our promise!
+            </p>
+            {!isConnected ? (
+                <div style={{textAlign: 'center', marginTop: '30px'}}>
+                    <Button shape="round" type="primary" style={styles.button} onClick={() => open()}>
+                        Connect to Submit Campaign
+                    </Button>
+                </div>
+            ) : (
+                <div style={{marginTop: '50px'}}>
+                    <ProgressSteps step={2}/>
+                    <Row style={{ marginTop: '30px' }}>
+                        <Col span={18} offset={3}>
+                            <Card title="Add Campaign to Marketplace" bordered={true}>
+                                <Form
+                                    name="basic"
+                                    labelCol={{span: 4}}
+                                    wrapperCol={{span: 18}}
+                                    onFinish={onFinish}
+                                    onFinishFailed={onFinishFailed}
+                                    autoComplete="off"
+                                >
+                                    <Form.Item<FieldType>
+                                        label="Project Budget"
+                                        name="budget"
+                                        rules={[{required: true, message: 'Input project budget'}]}
+                                    >
+                                        <InputNumber prefix={connectedChain?.nativeCurrency.symbol} style={{width: '100%'}}/>
+                                    </Form.Item>
 
-                            <Form.Item wrapperCol={{ offset: 10, span: 12 }} style={{ marginTop: '40px' }}>
-                                <Button type="primary" htmlType="submit" style={styles.button}>
-                                    Submit Campaign
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </Card>
-                </Col>
-            </Row>
+                                    <Form.Item wrapperCol={{ offset: 10, span: 12 }} style={{ marginTop: '40px' }}>
+                                        <Button type="primary" htmlType="submit" style={styles.button}>
+                                            Submit Campaign
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            )}
         </>
     );
 };
